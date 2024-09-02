18e4626d5be8234e0fd1fee3004773bf5f23f60d
bagasdwii <gasdwi1908@gmail.com>
2024-09-02 15:11:38 +0700

    test 8

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
+Route::get('/halo', function () {
+    return view('halo');
+});
+Route::get('/halo', function () {
+    return view('halo');
+});
 
