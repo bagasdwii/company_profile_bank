2ab17135a1df6370d5157b5346537966926cc490
bagasdwii <gasdwi1908@gmail.com>
2024-09-11 09:46:11 +0700

    CRUD_BERITA

---
+++ database/migrations/2024_09_11_011845_create_data_beritas.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_beritas', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->text('keterangan');
+            $table->string('gambar');
+            $table->date('tanggal');
+            $table->string('hari');
+            $table->foreignId('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_beritas');
+    }
+};
---
+++ resources/views/beritas/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.appv2')
+@section('title', 'Data Berita')
+@section('content')
+    <div class="container">
+        <a href="/data_berita" class="mb-3 btn btn-primary">Kembali</a>
+        <div class="row">
+            <div class="col-md-12">
+                <form  action="{{ route('data_berita.update', $dataBerita->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+                    <form-group>
+                        <label for="judul">Judul</label>
+                        <input type="text" id="judul" class="form-control" name="judul" placeholder="Judul"  value="{{ $dataBerita->judul }}">
+                    </form-group>
+                    <form-group>
+                        <label for="keterangan">Keterangan</label>
+                        <textarea type="text" id="keterangan" class="form-control" name="keterangan" placeholder="Keterangan"  >{{ $dataBerita->keterangan }}</textarea>
+                    </form-group>
+                    <div class="form-group">
+                        <label for="tanggal">Tanggal</label>
+                        <input type="date" id="tanggal" class="form-control" name="tanggal" value="{{ $dataBerita->tanggal}}">
+                    </div>
+                    <img class="img-fluid rounded mx-auto d-block my-3" src="/assets/data_berita/{{ $dataBerita->gambar }}" alt="">
+                    <form-group>
+                        <label for="gambar">Gambar</label>
+                        <input type="file" id="gambar" class="form-control" name="gambar"  >
+                    </form-group>
+                    <form-group>
+                        <button type="submit" class="btn btn-primary btn-block my-3">Submit</button>
+                    </form-group>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
