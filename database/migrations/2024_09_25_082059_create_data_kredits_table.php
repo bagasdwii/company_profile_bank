4cc8ad07405a9dc78b6a01d8dac669ab9efd4207
bagasdwii <gasdwi1908@gmail.com>
2024-09-25 15:31:42 +0700

    CRUD_KREDIT

---
+++ database/migrations/2024_09_25_082059_create_data_kredits_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_kredits', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('gambar'); 
+            $table->text('keterangan'); 
+            $table->string('nama_button'); 
+            $table->integer('nomor_button'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_kredits');
+    }
+};
